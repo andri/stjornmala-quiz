14791ca408cce11ffa1a31bf2fa0b0c4069862c8
Andri Sigurdsson <andrisig@gmail.com>
2021-08-06 22:32:29 +0000

    Fix

--- src/lib/questions.js
+++ src/lib/questions.js
@@ -23,140 +23,140 @@ export default [
     ],
     answer: null,
   },
-  // {
-  //   question: "4.",
-  //   answers: [
-  //     "Eftirlit og reglugerðar gera vanalega meiri skaða en gagn.",
-  //     "Opinbert eftirlit og lagareglur eru nauðsynlegar til að vernda almannahagsmuni.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "5.",
-  //   answers: [
-  //     "Mikill hagnaður er merki um góðan rekstur.",
-  //     "Hagnaður margra fyrirtækja er alltof mikill.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "6.",
-  //   answers: [
-  //     "Lög, reglugerðir og alþjóðasamningar til verndar umhverfinu geta dregið úr sköpun starfa.",
-  //     "Kostirnir við að þrengja að þeim sem mega eru alltaf fleiri en gallarnir.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "7.",
-  //   answers: [
-  //     "Innflytjendur sem vinna styrkja samfélagið en það eru alltof margir sem koma hingað til að leggjast upp á velferðarkerfið.",
-  //     "Innflytjendur styrkja alltaf samfélagið.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "8.",
-  //   answers: [
-  //     "Hin ríku eru rík vegna þess að þau hafa unnið fyrir því.",
-  //     "Auður og völd leggjast í ættir og eru oftar merki um inn í hvaða fjölskyldur fólk fæðist en verðleika einstaklinganna.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "9.",
-  //   answers: [
-  //     "Þeir úrgerðarmenn sem fá úthlutað mestum kvóta eru þeir sem hafa staðið sig best. Það er því gott að mest af kvótanum sé hjá þeim.",
-  //     "Kvótakerfið er ósanngjarnt og færir sífellt meira af kvótanum til þeirra sem hafa mest fyrir.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "10.",
-  //   answers: [
-  //     "Allt of margir komast upp með að leggja lítið til samfélagsins vegna þess að það er of auðvelt að lifa góðu lífi á bótum.",
-  //     "Fátækt fólk lifir allt of erfiðu lífi vegna þess að stuðningur samfélagsins er of veikur.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "11.",
-  //   answers: [
-  //     "Efnahagskerfið er í grunninn réttlátt, allir sem vilja geta nýtt tækifærin til að bæta stöðu sína.",
-  //     "Efnahagskerfið er ósanngjarnt og hyglir um of hagsmunum hinna fáu.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "12.",
-  //   answers: [
-  //     "Þrátt fyrir nokkra sigra er enn langt í land þar til konur fái jafnrétti á við karla á svo mörgum sviðum.",
-  //     "Í dag er engin munur á réttindum kynjanna, konur hafa náð jafnrétti í öllum helstu málum.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "13.",
-  //   answers: [
-  //     "Ísland er stéttlaust land.",
-  //     "Stéttaskipting er mikið mein í samfélaginu.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "14.",
-  //   answers: [
-  //     "Fólk sem er til í að leggja á sig vinnu og axla ábyrgð uppsker eins og það sáir.",
-  //     "Mikil vinna og einbeiting er engin tryggingin fyrir efnahagslegri velsæld.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "15.",
-  //   answers: [
-  //     "Verkalýðsfélögin eru vörn fólks gagnvart miklu valdi fyrirtækjaeigenda yfir starfsfólkinu.",
-  //     "Verkalýðsfélögin hafa alltof mikil völd, það er best að fólki semji sjálft um eigin kjör.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "16.",
-  //   answers: [
-  //     "Innflytjendur sem hingað flytja hafa sömu möguleika og innfæddir til að koma undir sig fótunum.",
-  //     "Fordómar og mismunun veldur því að það er mun erfiðara fyrir innflytjendur en innfædda að komast áfram í starfi eða taka virkan þátt í stjórnmálum eða öðru félagsstarfi.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "17.",
-  //   answers: [
-  //     "Samfélag er aðeins safn af einstaklingum. Það geta því ekki verið til nein samfélagsleg markmið, aðeins vilji og væntingar einstaklinganna.",
-  //     "Við eigum samfélagið saman og getum byggt upp gott og réttlátt samfélag saman.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "18.",
-  //   answers: [
-  //     "Hin fátæku bera alltaf mesta ábyrgð á stöðu sinni, fólk getur bætt hag sig ef það er duglegt og útsjónarsamt.",
-  //     "Fordómar gagnvart fátækum, t.d. öryrkjum, valda því að vandi þessa fólks verður verri en ella.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "19.",
-  //   answers: [
-  //     "Flöt skattprósenta er réttlát, skatturinn á ekki að refsa þeim sem standa sig betur og afla meiri tekna eða eiga meiri eignir.",
-  //     "Skattkerfið á að jafna tekjur fólks, hin betur settu eiga að borga hlutfallslega meira en hin sem hafa minna á milli handanna.",
-  //   ],
-  //   answer: null,
-  // },
-  // {
-  //   question: "20.",
-  //   answers: [
-  //     "Sterk fyrirtæki eru undirstaða samfélagsins og forsenda framþróunar þess.",
-  //     "Öflugt lýðræði og virk þátttaka almennings er grundvöllur góðs samfélags.",
-  //   ],
-  //   answer: null,
-  // },
+  {
+    question: "4.",
+    answers: [
+      "Eftirlit og reglugerðar gera vanalega meiri skaða en gagn.",
+      "Opinbert eftirlit og lagareglur eru nauðsynlegar til að vernda almannahagsmuni.",
+    ],
+    answer: null,
+  },
+  {
+    question: "5.",
+    answers: [
+      "Mikill hagnaður er merki um góðan rekstur.",
+      "Hagnaður margra fyrirtækja er alltof mikill.",
+    ],
+    answer: null,
+  },
+  {
+    question: "6.",
+    answers: [
+      "Lög, reglugerðir og alþjóðasamningar til verndar umhverfinu geta dregið úr sköpun starfa.",
+      "Kostirnir við að þrengja að þeim sem menga eru alltaf fleiri en gallarnir.",
+    ],
+    answer: null,
+  },
+  {
+    question: "7.",
+    answers: [
+      "Innflytjendur sem vinna styrkja samfélagið en það eru alltof margir sem koma hingað til að leggjast upp á velferðarkerfið.",
+      "Innflytjendur styrkja alltaf samfélagið.",
+    ],
+    answer: null,
+  },
+  {
+    question: "8.",
+    answers: [
+      "Hin ríku eru rík vegna þess að þau hafa unnið fyrir því.",
+      "Auður og völd leggjast í ættir og eru oftar merki um inn í hvaða fjölskyldur fólk fæðist en verðleika einstaklinganna.",
+    ],
+    answer: null,
+  },
+  {
+    question: "9.",
+    answers: [
+      "Þeir úrgerðarmenn sem fá úthlutað mestum kvóta eru þeir sem hafa staðið sig best. Það er því gott að mest af kvótanum sé hjá þeim.",
+      "Kvótakerfið er ósanngjarnt og færir sífellt meira af kvótanum til þeirra sem hafa mest fyrir.",
+    ],
+    answer: null,
+  },
+  {
+    question: "10.",
+    answers: [
+      "Allt of margir komast upp með að leggja lítið til samfélagsins vegna þess að það er of auðvelt að lifa góðu lífi á bótum.",
+      "Fátækt fólk lifir allt of erfiðu lífi vegna þess að stuðningur samfélagsins er of veikur.",
+    ],
+    answer: null,
+  },
+  {
+    question: "11.",
+    answers: [
+      "Efnahagskerfið er í grunninn réttlátt, allir sem vilja geta nýtt tækifærin til að bæta stöðu sína.",
+      "Efnahagskerfið er ósanngjarnt og hyglir um of hagsmunum hinna fáu.",
+    ],
+    answer: null,
+  },
+  {
+    question: "12.",
+    answers: [
+      "Þrátt fyrir nokkra sigra er enn langt í land þar til konur fái jafnrétti á við karla á svo mörgum sviðum.",
+      "Í dag er engin munur á réttindum kynjanna, konur hafa náð jafnrétti í öllum helstu málum.",
+    ],
+    answer: null,
+  },
+  {
+    question: "13.",
+    answers: [
+      "Ísland er stéttlaust land.",
+      "Stéttaskipting er mikið mein í samfélaginu.",
+    ],
+    answer: null,
+  },
+  {
+    question: "14.",
+    answers: [
+      "Fólk sem er til í að leggja á sig vinnu og axla ábyrgð uppsker eins og það sáir.",
+      "Mikil vinna og einbeiting er engin tryggingin fyrir efnahagslegri velsæld.",
+    ],
+    answer: null,
+  },
+  {
+    question: "15.",
+    answers: [
+      "Verkalýðsfélögin eru vörn fólks gagnvart miklu valdi fyrirtækjaeigenda yfir starfsfólkinu.",
+      "Verkalýðsfélögin hafa alltof mikil völd, það er best að fólki semji sjálft um eigin kjör.",
+    ],
+    answer: null,
+  },
+  {
+    question: "16.",
+    answers: [
+      "Innflytjendur sem hingað flytja hafa sömu möguleika og innfæddir til að koma undir sig fótunum.",
+      "Fordómar og mismunun veldur því að það er mun erfiðara fyrir innflytjendur en innfædda að komast áfram í starfi eða taka virkan þátt í stjórnmálum eða öðru félagsstarfi.",
+    ],
+    answer: null,
+  },
+  {
+    question: "17.",
+    answers: [
+      "Samfélag er aðeins safn af einstaklingum. Það geta því ekki verið til nein samfélagsleg markmið, aðeins vilji og væntingar einstaklinganna.",
+      "Við eigum samfélagið saman og getum byggt upp gott og réttlátt samfélag saman.",
+    ],
+    answer: null,
+  },
+  {
+    question: "18.",
+    answers: [
+      "Hin fátæku bera alltaf mesta ábyrgð á stöðu sinni, fólk getur bætt hag sig ef það er duglegt og útsjónarsamt.",
+      "Fordómar gagnvart fátækum, t.d. öryrkjum, valda því að vandi þessa fólks verður verri en ella.",
+    ],
+    answer: null,
+  },
+  {
+    question: "19.",
+    answers: [
+      "Flöt skattprósenta er réttlát, skatturinn á ekki að refsa þeim sem standa sig betur og afla meiri tekna eða eiga meiri eignir.",
+      "Skattkerfið á að jafna tekjur fólks, hin betur settu eiga að borga hlutfallslega meira en hin sem hafa minna á milli handanna.",
+    ],
+    answer: null,
+  },
+  {
+    question: "20.",
+    answers: [
+      "Sterk fyrirtæki eru undirstaða samfélagsins og forsenda framþróunar þess.",
+      "Öflugt lýðræði og virk þátttaka almennings er grundvöllur góðs samfélags.",
+    ],
+    answer: null,
+  },
 ];
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,8 @@
 <svelte:head>
-  <title>Ertu kommi, sósíalisti, krati eða kapítalisti?</title>
+  <title>Ertu illa farinn af nýfrjálshyggju?</title>
   <meta
     property="og:title"
-    content="Ertu kommi, sósíalisti, krati eða kapítalisti?"
+    content="Ertu illa farinn af nýfrjálshyggju?"
   />
   <meta property="og:url" content={`https://nyfrjalshyggja.x2021.is/`} />
   <meta property="og:image" content={`https://nyfrjalshyggja.x2021.is/share.png`} />
